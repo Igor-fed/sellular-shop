4598773543a721613e457aae26b25ed0dc2f98e0
Valentine <gladovskij123@gmail.com>
2026-02-04 12:36:36 +0300

    edit employee.php

--- employee.php
+++ employee.php
@@ -12,28 +12,31 @@
         <div class="language">
             <select>
                 <option value="ru" selected>RU</option>
-                <option value="en">EN</option>
+       
             </select>
         </div>
         <div class="login-link">
-            <a href="index.php">Выйти</a>
+            <a class="main-button" href="index.php">Выйти</a>
         </div>
     </div>
 </header>
 
 <main class="arm-container">
-    <div class="sidebar">
-        <button onclick="loadView('products')">Товары</button>
-        <button onclick="loadView('services')">Услуги</button>
-        <button onclick="loadView('clients')">Клиенты</button>
-        <button onclick="loadView('vw_sales_products')">Продажи товаров</button>
-        <button onclick="loadView('vw_sales_services')">Продажи услуг</button>
-        <button onclick="loadView('vw_report')">Отчёт</button>
+    <div class="sidebar" id="table-buttons">
+<button data-view="category">Категории</button>
+<button data-view="clients">Клиенты</button>
+<button data-view="contracts">Договоры</button>
+<button data-view="products">Товары</button>
+<button data-view="services">Услуги</button>
+<button data-view="sales">Продажи</button>
+<button data-view="sale_items">Позиции продаж</button>
+
     </div>
 
     <div class="content">
         <h2 id="table-title">Товары</h2>
         <table class="data-table" id="dataTable"></table>
+        <div id="procedure-controls" class="procedure-controls"></div>
     </div>
 </main>
 
